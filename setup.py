6fd4eb240f0e8df3ff40780ac5b7807dbd98d1b9
Fabian Büchler <fabian@buechler.io>
2014-01-21 22:26:35 +0100

    Make depdendency on mock and numpy optional for Python 3.3 and 3.4.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from setuptools import setup, find_packages
+import sys
 
 
 def read(path, strip=False):
@@ -32,6 +33,19 @@ classifiers = [
     for pyv in "2.6 2.7 3.0 3.1 3.2 3.3".split()
 ]
 
+
+PYMAJ = sys.version_info[0]
+PYMIN = sys.version_info[1]
+
+requirements = ['tabulate']
+# mock required for Python <= 3.3 (else unittest.mock)
+if PYMAJ < 3 or (PYMAJ == 3 and PYMIN < 3):
+    requirements.append('mock')
+# numpy required for Python <= 3.4 (else statistics)
+if PYMAJ < 3 or (PYMAJ == 3 and PYMIN < 4):
+    requirements.append('numpy')
+
+
 setup(
     name='analog',
     description='analog - Log Analysis Utility',
@@ -43,10 +57,7 @@ setup(
     author_email='fabian.buechler@gmail.com',
     entry_points={'console_scripts': ['analog=analog:main']},
     classifiers=classifiers,
-    install_requires=[
-        'numpy',
-        'tabulate',
-    ],
+    install_requires=requirements,
     packages=find_packages(),
     py_modules=['analog'],
     zip_safe=False,
