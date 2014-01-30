a5657581ac7bc3caf0f960da1d50918674fabb5f
Fabian Büchler <fabian@buechler.io>
2014-01-30 11:20:43 +0100

    Back to development.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from __future__ import (absolute_import, division, print_function,
 from setuptools import setup, find_packages
 
 
-VERSION = '0.2.0'
+VERSION = '0.2.1.dev'
 
 
 def read(path, strip=False):
